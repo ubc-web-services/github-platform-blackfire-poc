1cae1fa0ad386e924026d96b96b350358f0ed98f
Dave Kennedy <david.kennedy@ubc.ca>
2018-02-01 09:47:06 -0800

    new project

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -41,15 +41,3 @@ if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
-$databases['default']['default'] = array (
-  'database' => 'drupal8',
-  'username' => 'drupal8',
-  'password' => 'drupal8',
-  'prefix' => '',
-  'host' => 'database',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'dadmw7XzfYsHeJ7tWQfp_ibrz4BKAYYtOoqL3yFa6IWhroVPLb00qFwOirrk-3VKzZDoyK1f4w';
-$settings['install_profile'] = 'standard';
